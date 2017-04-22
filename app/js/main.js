fe23b43d4a125b15407a89eeec9158cf8fd8f906
ekliziast2014 <ekliziast2014@gmail.com>
2017-04-22 17:39:15 +0300

    All pages DONE V 0.1.0

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,28 @@
-var elem = $('.start');
-$(document).ready(function () {
-    console.log( 'Привет, рад видеть ! Анимация подключена !' );
-    elem.addClass('show myAnimation slideInDown');
-
-});
+var elemSt = $('.start');
+// Для проверки 1-я ф-ция должна быть закоментирована
+// $(document).ready(function () {
+//     console.log( 'Привет, рад видеть ! Анимация подключена !' );
+//     elemSt.addClass('show myAnimation slideInDown');
+//
+// });
 $('#switch').click(function () {
-    console.log( 'KKKK' );
     $('body').addClass('content');
-    elem.removeClass('show');
+    elemSt.removeClass('show');
+    $('.technic').addClass('show');
+    $('.header').addClass('show');
     $('.about').addClass('show');
     $('.footer').addClass('show');
-
+});
+$('.me').click(function () {
+    $('section[class !=".about"]').removeClass('show');
+    $('.about').addClass('show');
+});
+$('.path').click(function () {
+    $('section[class !=".career"]').removeClass('show');
+    $('.career').addClass('show');
+});
+$('.portf').click(function () {
+    $('section[class !=".portfolio"]').removeClass('show');
+    $('.portfolio').addClass('show');
 });
 
-/*
-$('#switch').click(function () {
-    console.log( 'KKKK' );
-    $('body').addClass('content');
-    $('start').style.display = "none";
-});
-*/
