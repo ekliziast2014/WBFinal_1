3937e243b19eebcfac90dad3713ec0e7eed50f72
ekliziast2014 <ekliziast2014@gmail.com>
2017-04-30 18:07:53 +0300

    adaptive-DONE SASS-DONE

--- gulpfile.js
+++ gulpfile.js
@@ -14,22 +14,35 @@ gulp.task('connect', function () {
     });
     opn('http://localhost:555');
 });
-
+/*// ---------------Первая версия сначала компиляция потом конкатенация в main.css
 // Компиляция SCSS в CSS
 gulp.task('sass', function () {
-    gulp.src('app/scss/**/*.scss')
+    gulp.src('app/scss/**!/!*.scss')
         .pipe(sass())
         .pipe(gulp.dest('app/css/vendor'));
 });
 
 // Конкатенация CSS в один файл main.css
 gulp.task('concat', function () {
-    gulp.src('app/css/vendor/*.css')
+    gulp.src('app/css/vendor/!*.css')
         .pipe(sourcemaps.init())
         .pipe(concat('main.css'))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('app/css'));
 });
+// ---------------------Конец первой версии---------------------------*/
+
+// ----------------------Вторая версия - РАБОЧАЯ: компиляция без конкатенации ------------------
+gulp.task('sass', function () {
+    gulp.src('app/scss/**/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('app/css'));
+});
+
+
+// ---------------Конец второй версии--------------------
 
 // Работа с HTML
 gulp.task('html', function () {
@@ -58,6 +71,8 @@ gulp.task('watch', function () {
     gulp.watch(['app/js/**/*.js'], ['js']);
 });
 
-// Задача по-умолчанию
-gulp.task('default', ['sass','concat','connect', 'watch']);
-// Полная проверка добавить такие таски -  ,'connect', 'watch'
+/*// Задача по-умолчанию для первой версии
+gulp.task('default', ['sass','concat','connect', 'watch']);*/
+
+// Задача по-умолчанию для второй версии - РАБОЧАЯ!
+gulp.task('default', ['sass','connect', 'watch']);
